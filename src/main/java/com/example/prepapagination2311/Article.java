48ed70dc54be3a4d50dcd64d5b8cd9880abd149b
abre0801 <redouan080198@hotmail.com>
2023-11-23 16:38:13 +0100

    AJOUT GETTER SETTER POUR ARTICLE

--- src/main/java/com/example/prepapagination2311/Article.java
+++ src/main/java/com/example/prepapagination2311/Article.java
@@ -12,7 +12,32 @@ public class Article {
     private Long id;
 
     private String title;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
     private String content;
+
+    public Long getId() {
+        return id;
+    }
 
     // Getters and setters
 }
